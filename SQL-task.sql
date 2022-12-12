7012a97f7d47b39b40c15bc922ba2f42b709ee59
lubnaajlouni <115452696+lubnaajlouni@users.noreply.github.com>
2022-12-12 16:40:13 +0300

    Add files via upload

---
+++ SQL-task.sql
@@ -0,0 +1,33 @@
+create table customers (
+id int,
+fname varchar(20),
+lname varchar(20),
+age int,
+);
+SELECT * FROM Customers;
+
+
+create table orders (
+id int,
+price float,
+product varchar(20),
+total float,
+);
+SELECT * FROM orders;
+
+
+create table Products(
+id int,
+product varchar(20),
+productPrice float,
+);
+SELECT * FROM Products;
+
+
+create table Employees(
+id int,
+number int,
+fname varchar(20),
+);
+SELECT * FROM Employees;
+
